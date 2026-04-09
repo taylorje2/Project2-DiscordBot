858224a9f945d4da5bc85ab2a431455ebd6a5af9
Terysa <tandara@etsu.edu>
2026-04-09 14:24:53 -0400

    Create zodiac.py

---
+++ zodiac.py
@@ -0,0 +1,6 @@
+# I think we talked about doing a drop down box for this... So the user input will have to change... I just wanted to get something done for the loop first.
+
+userBirthday = input ("Please enter your date of birth (MM/DD): ")
+
+for userBirthday in birthdays:
+    
