a3381210763a77890d712a4787aad7010e29e55a
smithra8-ui <smithra8@etsu.edu>
2026-04-12 17:34:36 -0400

    Update main.py
    I added somethings so that commands to the bot would work

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # pip install -U discord.py
 import discord 
+from discord.ext import commands
+import logging
 import os
 
 # I don't remember how to do this exactly, but I'll look in my notes... Desjardins just showed us how to do this in Advanced Database yesterday...
@@ -8,6 +10,17 @@ from dotenv import load_dotenv
 # Load the .env file and get the DISCORD_TOKEN variable
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+#^ create a log file, so that if an error occurs we can see which method is going wrong
+intents = discord.Intents.default()
+intents.message_content = True # allows bot to send message
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+#^any commands sent bot start with '/', for instance '/horoscope'
+
+@bot.event
+async def horoscope(ctx):
+    await ctx.send(f'')
 
 # Create a new Discord client and set up event handlers
 client = discord.Client()
