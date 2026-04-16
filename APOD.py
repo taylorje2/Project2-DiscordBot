5979ddac43a0d5a85b96cd27583d21afca292ecf
Jayden <taylorje2@etsu.edu>
2026-04-15 20:52:15 -0400

    Completed command 1/2 for APOD

--- APOD.py
+++ APOD.py
@@ -28,3 +28,34 @@ intents.members = True
 
 # set command prefix
 bot = commands.Bot(command_prefix='/', intents=intents)
+
+# command for the current day's APOD (/apod)
+@bot.command()
+async def apod(ctx):
+    # parameters for the API request
+    params = {
+        "api_key": NASA_API_KEY
+    }
+
+    # make the API request and get the response
+    apod_response = requests.get(BASE_URL, params=params)
+
+    # check if the request was successful
+    if apod_response.status_code == 200:
+        # parse the response JSON into a nasa_apod object
+        apod_data = nasa_apod(**apod_response.json())
+
+        # reply to user with the APOD data in an embed message
+        await ctx.send(f"Here's the Astronomy Picture of the Day for {str(apod_data.date)}:")
+
+        # create an embed message with the APOD data
+        embed = discord.Embed(title=apod_data.title, description=apod_data.explanation)
+        embed.set_image(url=apod_data.url)
+
+        # send the embed message to the channel
+        await ctx.send(embed=embed)
+    else:
+        await ctx.send("Sorry, I couldn't fetch the APOD for today. \nPlease try again later.")
+
+# run the bot
+bot.run(TOKEN, log_handler=handler, log_level=logging.INFO)
