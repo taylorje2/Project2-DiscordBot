053ca4278a597d330f32f545e160016c2698f32a
Jayden <taylorje2@etsu.edu>
2026-04-21 18:24:29 -0400

    Added the /oldapod command

--- APOD.py
+++ APOD.py
@@ -58,6 +58,40 @@ async def apod(ctx):
     else:
         await ctx.send("Sorry, I couldn't fetch the APOD for today. \nPlease try again later.")
 
+# command for past APOD
+@bot.command()
+async def oldapod(ctx, date: str = None):
+    # pleaceholder text to let the user know that data is being fetched
+    placeholder = await ctx.send("**Fetching the APOD... This might take a second...**")
+
+    # make the bot look like it's typing 
+    async with ctx.typing() :
+        # parameters for the API request
+        params = {
+            "api_key": NASA_API_KEY,
+            "date": date
+        }
+
+        # API request
+        apod_response = requests.get(BASE_URL, params=params)
+
+        # check if the response was successful
+        if apod_response.status_code == 200:
+            # parse the response JSON into a nasa_apod object
+            apod_data = nasa_apod(**apod_response.json())
+
+            # reply to user with the APOD data in an embed message
+            await ctx.send(f"Here's the APOD for {str(apod_data.date)}, {ctx.author.mention}:")
+
+            # create an embed message with the APOD data
+            embed = discord.Embed(title=apod_data.title, description=apod_data.explanation)
+            embed.set_image(url=apod_data.url)
+
+            # send the embed message to the channel
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send(f"Sorry, I couldn't fetch the APOD for {str(apod_data.date)}. \nPlease try again later.")
+
 # run the bot
 bot.run(TOKEN2, log_handler=handler, log_level=logging.INFO)
 
@@ -65,19 +99,3 @@ bot.run(TOKEN2, log_handler=handler, log_level=logging.INFO)
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}")
-
-
-###################################################################################
-# DELETE THIS LATER, TESTING PURPOSES ONLY, NOT PART OF FINAL BOT FUNCTIONALITY
-###################################################################################
-# reply to hello message 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if "hello" in message.content.lower():
-        await message.channel.send(f"Hello {message.author.mention}!")
-
-    # respond to all other messages sent
-    await bot.process_commands(message)
