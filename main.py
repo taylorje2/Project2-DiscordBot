2d21d4699bcfefe921fa3dd011b212247c85ac2d
Jayden <taylorje2@etsu.edu>
2026-04-26 18:52:51 -0400

    not sure

--- main.py
+++ main.py
@@ -26,7 +26,10 @@ intents.message_content = True # allows bot to send message
 
 
 bot = commands.Bot(command_prefix='/', intents=intents)
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 5569d1a (Added the bot tree for commands list)
 #^any commands sent bot start with '/', for instance '/horoscope'
 tree = bot.tree
 
