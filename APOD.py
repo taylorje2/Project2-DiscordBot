06f5fc04a2d1669cab70cfd907e690b0d848eadf
Jayden <taylorje2@etsu.edu>
2026-04-26 16:28:23 -0400

    APOD update from Jayden branch for bot details

--- APOD.py
+++ APOD.py
@@ -3,6 +3,7 @@ from fromapis import nasa_apod
 from dotenv import load_dotenv
 import os
 import discord 
+import logging
 from discord.ext import commands
 
 load_dotenv()
@@ -11,3 +12,23 @@ NASA_API_KEY = os.getenv("NASA_API_KEY")
 
 BASE_URL = "https://api.nasa.gov/planetary/apod"
 
+# token specifically for apod bot
+TOKEN2 = os.getenv("APOD_BOT_TOKEN")
+
+# writes all activity into discord.log file (for debug)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+
+# permissions for the bot
+intents = discord.Intents.default()
+
+# allows bot to send messages
+intents.message_content = True 
+
+# allows bot access to server member data
+intents.members = True
+
+# set command prefix
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+# command list tree (shows a list of bot commands when "/" is entered)
+tree = bot.tree
