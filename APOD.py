415e90f72f563e17127e63428601eba67728d243
Jayden <taylorje2@etsu.edu>
2026-04-26 16:43:32 -0400

    Updated to the correct API key name

--- APOD.py
+++ APOD.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 
 load_dotenv()
 
-NASA_API_KEY = os.getenv("NASA_API_KEY")
+NASA_API_KEY = os.getenv("APOD_API_KEY")
 
 BASE_URL = "https://api.nasa.gov/planetary/apod"
 
