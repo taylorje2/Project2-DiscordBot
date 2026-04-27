0e3d5721875af01d9c207f785e446e2477c7c962
Terysa <tandara@etsu.edu>
2026-04-26 21:54:06 -0400

    Update confirm.py

--- confirm.py
+++ confirm.py
@@ -17,16 +17,3 @@ class Confirm (discord.ui.View):
         self.value = False
         self.stop()
         await interaction.response.defer()
-
-
-
-
-
-
-
-#-------------------------- Outside Resources --------------------------
-# ui button -- https://discordpy.readthedocs.io/en/stable/interactions/api.html#discord.ui.Button.view
-# ui button -- https://guide.pycord.dev/interactions/ui-components/buttons#:~:text=import%20discord,class%20that%20contains%20the%20button
-# ui button -- https://stackoverflow.com/questions/79653645/how-to-make-a-confirmation-prompt-with-buttons-with-a-discord-bot-in-discord-py
-# interaction response -- https://discordpy.readthedocs.io/en/latest/interactions/api.html#discord.InteractionResponse
-# emoji unicode -- https://gist.github.com/chrisurf/a372d3e7de5371401b23217533a9476a
