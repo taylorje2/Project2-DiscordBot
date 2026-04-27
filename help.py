d4f18f51dee1333636142ac1c0598df5bfcf2c7d
Terysa <tandara@etsu.edu>
2026-04-26 22:49:08 -0400

    Update help.py

--- help.py
+++ help.py
@@ -6,15 +6,17 @@ def helpcommand(bot):
     async def help(interaction: discord.Interaction):
         embed = discord.Embed(title="Bot Commands", description="Here are the available bot commands", color=discord.Color.pink())
         embed.add_field(name="/newuser", value="Create a new User", inline=False)
-        embed.add_field(name="/getuserinfo", value="Create a new User", inline=False)
-        embed.add_field(name="/updateusername", value="Create a new User", inline=False)
-        embed.add_field(name="/updateuserzodiac", value="Create a new User", inline=False)
-        embed.add_field(name="/delete", value="Create a new User", inline=False)
-        embed.add_field(name="/horoscope", value="Create a new User", inline=False)
-        embed.add_field(name="/moon", value="Create a new User", inline=False)
-        embed.add_field(name="/apod", value="Create a new User", inline=False)
-        embed.add_field(name="/oldapod", value="Create a new User", inline=False)
+        embed.add_field(name="/getuserinfo", value="view user information - username and zodiac sign", inline=False)
+        embed.add_field(name="/updateusername", value="update username", inline=False)
+        embed.add_field(name="/updateuserzodiac", value="update zodiac sign", inline=False)
+        embed.add_field(name="/delete", value="Delete user", inline=False)
+        embed.add_field(name="/horoscope", value="Get your daily horoscope", inline=False)
+        embed.add_field(name="/moon", value="Get Today's Moon Phase", inline=False)
+        embed.add_field(name="/apod", value="Get the NASA APOD for today", inline=False)
+        embed.add_field(name="/oldapod", value="Get the NASA APOD for a past date", inline=False)
         embed.add_field(name="/help", value="Create a new User", inline=False)
+        
+        embed.set_footer(text="Tip: Use /help to view commands anytime")
 
 
 #-------------------------- Outside Resources --------------------------
